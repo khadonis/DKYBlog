379472a7e4f2e31413fece8eddaed12cca0b9489
khadonis <kaan.demirkol@outlook.com>
2017-07-25 18:02:42 +0300

    İnstagram slider yapımına başlanacak.

--- app/js/main.js
+++ app/js/main.js
@@ -25,3 +25,54 @@ menuBtn.click(function (evt) {
 menuClose.click(function () {
     navMenu.removeClass('active');
 });
+
+//sliders
+var slideThumb = $('.nb-img'), slideTitle = $('.ng-txt'), mainSlide = $('.main-swiper-container .swiper-slide'), slideBtnTxt = $('.nb-txt'), indexX, slideImg, slideImgtitle;
+
+
+
+function slideBtnNextAct(index) {
+    slideImg = mainSlide.eq(index).find('img').attr('src');
+    slideImgtitle = mainSlide.eq(index).find('img').attr('alt');
+    slideThumb.css('background-image', 'url(' + slideImg + ')');
+    slideBtnTxt.empty().text(slideImgtitle);
+}
+slideBtnNextAct(1);
+
+
+var mainSwiper = new Swiper('.main-swiper-container', {
+    slidesPerView: 'auto',
+   // loopedSlides: 1,
+    //loop: true,
+    centeredSlides: true,
+    nextButton: '.main-swiper-button-next',
+    prevButton: '.main-swiper-button-prev',
+    autoplay: 4000,
+    runCallbacksOnInit: true,
+
+    onSlideChangeStart: function () {
+        indexX = mainSwiper.realIndex + 1;
+        slideBtnNextAct(indexX);
+        if (slideImg == '' || slideImg == undefined) {
+            slideThumb.width(0);
+            slideThumb.css('min-width', '0');
+        } else {
+            slideThumb.width('auto');
+            slideThumb.css('min-width', '150px');
+        }
+        if (slideImgtitle == '' || slideImgtitle == undefined) {
+            slideBtnTxt.css('padding', '0');
+        } else {
+            slideBtnTxt.css('padding', '30px');
+        }
+    }
+
+});
+var newsSwiper = new Swiper('.news-swiper-container', {
+    paginationClickable: true,
+    nextButton: '.news-swiper-button-next',
+    prevButton: '.news-swiper-button-prev',
+    direction: 'vertical',
+    autoplay: 4000
+});
+
